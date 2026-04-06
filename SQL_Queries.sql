f36f242a4cddc0626783bba2b62e3c863ee4b761
Luca Duong <luca.mediawork@gmail.com>
2026-04-06 15:53:31 +0700

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -46,3 +46,63 @@ group by member_id
 order by 2 desc
 limit 5
 ;
+
+-- claim type break down cost for each high-cost member
+
+with total_member as (
+	select member_id, sum(paid_amount) as total_paid
+    from claims
+    group by member_id),
+top_member as (
+	select member_id
+    from total_member
+    order by total_paid desc
+    limit 5)
+select c.member_id, c.claim_type, sum(c.paid_amount) as total_paid_cost
+from claims c
+join top_member t
+on c.member_id = t.member_id
+join total_member tm
+on c.member_id = tm.member_id
+group by c.member_id, c.claim_type
+order by tm.total_paid desc, total_paid_cost desc
+;
+
+-- 4. Billed vs Paid Ratio
+
+-- overall paid ratio
+select
+	sum(paid_amount),
+	sum(billed_amount),
+    round(sum(paid_amount) * 1.0 / nullif(sum(billed_amount),0), 2) as paid_ratio
+from claims;
+
+-- average paid ratios by: claim_type
+select
+	claim_type,
+    sum(paid_amount),
+    sum(billed_amount),
+	round(sum(paid_amount) * 1.0 / nullif(sum(billed_amount),0), 2) as paid_ratio
+from claims
+group by claim_type
+;
+
+-- average paid ratios by: provider
+select
+	provider_id,
+    sum(paid_amount),
+    sum(billed_amount),
+	round(sum(paid_amount) * 1.0 / nullif(sum(billed_amount),0), 2) as paid_ratio
+from claims
+group by provider_id
+;
+
+-- average paid ratios by: CPT code
+select
+	cpt_code,
+    sum(paid_amount),
+    sum(billed_amount),
+	round(sum(paid_amount) * 1.0 / nullif(sum(billed_amount),0), 2) as paid_ratio
+from claims
+group by cpt_code
+;
